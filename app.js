e07efd089fc785b50e9ccb8f46b190633989321d
Anastasiia <anas.simonova@gmail.com>
2021-05-07 12:04:33 +0200

    number

---
+++ app.js
@@ -0,0 +1,31 @@
+//1
+pi = Math.PI.toFixed(2);
+console.log(pi);
+
+//2
+min = Math.min (15, 11, 16, 12, 51, 12, 13, 51);
+console.log(min);
+
+max = Math.max (15, 11, 16, 12, 51, 12, 13, 51);
+console.log(max);
+
+//3
+rand = Math.random().toFixed(2);
+console.log(rand);
+
+rand2 = Math.floor(Math.random() * 10);
+console.log(rand2);
+
+//4
+let a = (0.6 + 0.7).toFixed(1);
+console.log(a);
+
+//5
+
+a = parseInt('100$');
+console.log(a);
+console.log(typeof a);
+
+const X = 20;
+const randomX = Math.round(Math.random() * X);
+console.log(randomX);
