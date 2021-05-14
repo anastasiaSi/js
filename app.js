d22c056b3a28de5a8ec01d5bceb842b9d769ace0
Anastasiia <anas.simonova@gmail.com>
2021-05-14 17:38:30 +0200

    string

--- app.js
+++ app.js
@@ -1,31 +1,64 @@
-//1
-pi = Math.PI.toFixed(2);
-console.log(pi);
+/* 1. Получить первую и последнюю буквы строки
+let string = 'some test string';
+let first;
+let last;
+first = string[0];
+last = string[string.length - 1];
+console.log(first, last);*/
 
-//2
-min = Math.min (15, 11, 16, 12, 51, 12, 13, 51);
-console.log(min);
+/* 2. Сделать первую и последнюю буквы в верхнем регистре
+let string = 'some test string';
+let first;
+let last;
+first = string[0];
+first = first.toLocaleUpperCase();
+last = string[string.length - 1];
+last = last.toLocaleUpperCase();
+console.log(first, last);
+*/
 
-max = Math.max (15, 11, 16, 12, 51, 12, 13, 51);
-console.log(max);
+/*3. Найти положение слова ‘string’ в строке
+let string = 'some test string';
+let value;
 
-//3
-rand = Math.random().toFixed(2);
-console.log(rand);
+value = string.indexOf('string') ;
+console.log(value);
+*/
+/*4. Найти положение второго пробела (“вручную” ничего не считать)
+let string = 'some test string';
+let value;
+value = string.lastIndexOf(' ');
+console.log(value);
+*/
 
-rand2 = Math.floor(Math.random() * 10);
-console.log(rand2);
+/* 5. Получить строку с 5-го символа длиной 4 буквы
+let string = 'some test string';
+let value;
+value = string.substr(5, 4);
+console.log(value);
+*/
+/*6. Получить строку с 5-го по 9-й символы
+let string = 'some test string';
+let value;
+value = string.slice(5, 9);
+console.log(value);
+*/
+/*7. Получить новую строку из исходной путем удаления последних 6-и символов
+//(то есть исходная строка без последних 6и символов)
+let string = 'some test string';
+let value;
+value = string.slice(0, -6);
+console.log(value);
+*/
+//8. Из двух переменных a=20 и b=16 получить переменную string, в которой будет
+//содержаться текст “2016”
 
-//4
-let a = (0.6 + 0.7).toFixed(1);
-console.log(a);
+let a = 20;
+let b = 16;
+let string;
 
-//5
+a = String(a);
+string = a + b;
+console.log(string);
+console.log(typeof string);
 
-a = parseInt('100$');
-console.log(a);
-console.log(typeof a);
-
-const X = 20;
-const randomX = Math.round(Math.random() * X);
-console.log(randomX);
