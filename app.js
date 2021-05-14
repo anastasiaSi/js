3205f8916045afa2db921adca96ed613a2e58495
Anastasiia <anas.simonova@gmail.com>
2021-05-14 18:56:24 +0200

    object

--- app.js
+++ app.js
@@ -1,64 +1,46 @@
-/* 1. Получить первую и последнюю буквы строки
-let string = 'some test string';
-let first;
-let last;
-first = string[0];
-last = string[string.length - 1];
-console.log(first, last);*/
-
-/* 2. Сделать первую и последнюю буквы в верхнем регистре
-let string = 'some test string';
-let first;
-let last;
-first = string[0];
-first = first.toLocaleUpperCase();
-last = string[string.length - 1];
-last = last.toLocaleUpperCase();
-console.log(first, last);
-*/
-
-/*3. Найти положение слова ‘string’ в строке
-let string = 'some test string';
+const user = {
+firstName: 'Anastasiia',
+age: 28,
+isAdmin: true,
+email: 'test@test.com',
+'user-address':{
+  city: 'Stuttgart'
+},
+skills: ['html', 'css', 'js']
+};
 let value;
+let prop ='skills';
 
-value = string.indexOf('string') ;
-console.log(value);
-*/
-/*4. Найти положение второго пробела (“вручную” ничего не считать)
-let string = 'some test string';
-let value;
-value = string.lastIndexOf(' ');
-console.log(value);
-*/
+value = user.firstName;
+value = user['isAdmin'];
+value = user['user-address'];
+value = user['user-address'].city;
+value = user['user-address']['city'];
+value = user[prop][0];
 
-/* 5. Получить строку с 5-го символа длиной 4 буквы
-let string = 'some test string';
-let value;
-value = string.substr(5, 4);
-console.log(value);
-*/
-/*6. Получить строку с 5-го по 9-й символы
-let string = 'some test string';
-let value;
-value = string.slice(5, 9);
-console.log(value);
-*/
-/*7. Получить новую строку из исходной путем удаления последних 6-и символов
-//(то есть исходная строка без последних 6и символов)
-let string = 'some test string';
-let value;
-value = string.slice(0, -6);
-console.log(value);
-*/
-//8. Из двух переменных a=20 и b=16 получить переменную string, в которой будет
-//содержаться текст “2016”
+user.firstName = 'Nastya'
+value = user.firstName;
+user.info = 'Some info';
 
-let a = 20;
-let b = 16;
-let string;
+value = user.info;
 
-a = String(a);
-string = a + b;
-console.log(string);
-console.log(typeof string);
+user['user-address'].city = 'Novocherkassk';
+user['user-address'].country = 'Russland';
 
+
+console.log(value);
+console.log(user);
+
+
+
+const obj = {
+product: 'iphone'
+};
+
+obj.price = 1000;
+obj.currency = 'dollar';
+obj.details = {};
+obj.details.model = '8';
+obj.details.color = 'red';
+
+console.log(obj);
