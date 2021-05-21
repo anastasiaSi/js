d0a2a8d149c560e065d6658d21fa1d2aa9e790bd
Anastasiia <anas.simonova@gmail.com>
2021-05-21 12:57:42 +0200

    Array methods

--- app.js
+++ app.js
@@ -1,70 +1,83 @@
 /*
-function sayHello(firstName, lastName) {
-  console.log(firstName, lastName);
-  console.log('Hello world');
-  return `Hello ${firstName} ${lastName}`;
+function getArray(num){
+  let arr = [];
+  for  (i = 1 ; i <= num; i++) {
+    arr.push(i);
+    
+  }
+  return arr;
 }
-let res1 = sayHello('Nastya', 'Simonova');
-let res2 = sayHello('Nastya', 'Simonova');
-console.log(res2);
+console.log (getArray (10));
 
-function multiply() {
-  if (arguments.length === 0) return 0;
-  let res = 1;
-  for (i = 0; i < arguments.length; i++){
-    res *= arguments[i];
+getArray(10);
+
+
+function doubleArray(arr) {
+ let res = [];
+ res = arr.concat(arr);
+ console.log(res);
+ return res;
+}
+
+doubleArray([1,2,3]) // [1,2,3,1,2,3] 
+
+function changeCollection() {
+  let res = [];
+
+  for (let i = 0; i < arguments.length; i++) {
+    if (Array.isArray(arguments[i])) {
+      const elements = arguments[i].slice();
+      elements.shift();
+      res.push(elements)
+    }
+  }
+  return res;
+}
+
+console.log(changeCollection([1,2,3])) */
+
+const users = [
+  {
+    "_id": "5e36b779dc76fe3db02adc32",
+    "balance": "$1,955.65",
+    "picture": "http://placehold.it/32x32",
+    "age": 33,
+    "name": "Berg Zimmerman",
+    "gender": "male"
+  },
+  {
+    "_id": "5e36b779d117774176f90e0b",
+    "balance": "$3,776.14",
+    "picture": "http://placehold.it/32x32",
+    "age": 37,
+    "name": "Deann Winters",
+    "gender": "female"
+  },
+  {
+    "_id": "5e36b779daf6e455ec54cf45",
+    "balance": "$3,424.84",
+    "picture": "http://placehold.it/32x32",
+    "age": 36,
+    "name": "Kari Waters",
+    "gender": "female"
+  }
+]
+
+
+function filterUsers(arr, key, value) {
+
+  let res = [];
+  if (!Array.isArray(arr)) return new Error('Это не массив');
+  if (typeof key !== "string" || key === '') return new Error('Входные данные не верны');
+  if (value === undefined || value === null) return new Error('Данные не верны, введите число');
+
+  for (i = 0; i < arr.length; i++) {
+    if (arr[i][key] === value) {
+      res.push(arr[i])
+    }
   }
 
-  console.log(res);
   return res;
-};
-multiply(2, 4, 5, 6);
-multiply();
-
-
-function reverseString(str) {
-  let string = String(str);
-  let res = '';
-  for (let i = string.length - 1; i >= 0; i--){
-    res += string[i];
-  } 
-  console.log(res);
-  return res; 
-};
-
-reverseString('test');
-reverseString(' ');
-reverseString(null);
-reverseString(undefined);
-reverseString();
-
-
-function getCodeStringFromText(str) {
-  let res = '';
-    for (i = 0; i < str.length; i ++) {
-      console.log(str[i]);
-      res += str[i].charCodeAt() + ' ';
-      
-      
-    }
-    console.log(res);
-    return res.trim();
-    
 }
 
-getCodeStringFromText("hello");*/
-
-function guessTheNumber(num) {
-  let number = Number(num);
-
-  if (typeof number !== "number" || isNaN(number)) return new Error("Please provide a valid number");
-  if (number < 0 || number > 10) return new Error("Please provide number in range 0 - 10");
-
-  let random = Math.ceil(Math.random() * 10);
-
-  if (random === number) return "You win!";
-
-  return `You are lose, your number is ${number}, the random number is ${random}`;
-}
-
-console.log(guessTheNumber(5)); 
+console.log(filterUsers(users, "age", 36))
