dd0e9943e79e69c8e32d122ab540cf27836aa52e
Anastasiia <anas.simonova@gmail.com>
2021-07-09 15:07:25 +0200

    Arrow functions

--- app.js
+++ app.js
@@ -1,92 +1,14 @@
-const rectangle = {
-  width: 35, 
-  height:4, 
-  getSquare() {
-    return this.width * this.height;
-  }};
-
-  console.log(rectangle.getSquare());
-  //2
-  const price = {
-    price: 10,
-    discount: '15%',
-    getPrice() {
-      return this.price;
-      
-    },
-    getPriceWithDiscount() {
-      const discount = parseFloat(this.discount);
-      const priceWithDiscount = this.price - (this.price * (discount / 100));
-      return priceWithDiscount;
-    }};
-
- //3
-const heightPlus = {
-  height: 10,
-  sum(){
-    this.height += 1;
-    return this.height;
-  }
-}
-
-//4
-
-const numerator = {
-  value: 1,
-  double() {
-    this.value *= 2;
-    return this;
-  },
-  plusOne() {
-    this.plusOne += 1;
-    return this;
-  },
-  minusOne() {
-    this.minusOne -= 1;
-    return this;
-  },
-  }
-  numerator.double().plusOne().minusOne();
-  console.log(numerator.value);
-  
-  //5 - 6
-  let product1 = {
-    price : 4,
-    counts: 100,
-    getPrice(){
-      return this.price * this.counts;
-    } 
-  }
-
-  let product2 = {
-    price : 5,
-    counts: 20,
-  }
-product1.getPrice.call(product2);
-console.log(product1.getPrice.call(product2));
-
-//7 
-
-let sizes = {
-  width: 5, 
-  height: 10
-},
-getSquare = function () {
-  return this.width * this.height
+//1
+const sum = () => {
+  if (!params.length) return 0;
+  return params.reduce((prev, next) => prev + next);
 };
-getSquare.call(sizes);
-console.log(getSquare.call(sizes));
 
-//8 
-let element = {
-  height: 25,
-  getHeight: function () {
-    return this.height;
-  }
-  };
-  
-let getElementHeight = element.getHeight.bind(element);
-getElementHeight(); // undefined
-console.log(getElementHeight());
+//2
 
-  
+
+const convertToObject = (num) => ({
+  value: num,
+  isOdd: Boolean(num % 2)
+}); 
+
